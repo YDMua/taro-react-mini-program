b4ccfa002ef2b8416d631e2eb4a2bfe50cb1e53d
YDMua <smallant175@163.com>
2018-09-18 09:24:20 +0800

    创建项目

---
+++ src/app.tsx
@@ -0,0 +1,51 @@
+import Taro, { Component, Config } from '@tarojs/taro'
+import '@tarojs/async-await'
+import { Provider } from '@tarojs/redux'
+
+import Index from './pages/index'
+
+import configStore from './store'
+
+import './app.scss'
+
+const store = configStore()
+
+class App extends Component {
+
+  /**
+   * 指定config的类型声明为: Taro.Config
+   *
+   * 由于 typescript 对于 object 类型推导只能推出 Key 的基本类型
+   * 对于像 navigationBarTextStyle: 'black' 这样的推导出的类型是 string
+   * 提示和声明 navigationBarTextStyle: 'black' | 'white' 类型冲突, 需要显示声明类型
+   */
+  config: Config = {
+    pages: [
+      'pages/index/index'
+    ],
+    window: {
+      backgroundTextStyle: 'light',
+      navigationBarBackgroundColor: '#fff',
+      navigationBarTitleText: 'WeChat',
+      navigationBarTextStyle: 'black'
+    }
+  }
+
+  componentDidMount () {}
+
+  componentDidShow () {}
+
+  componentDidHide () {}
+
+  componentCatchError () {}
+
+  render () {
+    return (
+      <Provider store={store}>
+        <Index />
+      </Provider>
+    )
+  }
+}
+
+Taro.render(<App />, document.getElementById('app'))
